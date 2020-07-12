c36b801f8654962694b51bd83a0e91193361e4f5
Fernando Maldonado <fernandomal@hotmail.com>
2020-07-12 22:06:15 +0100

    Add Pascal Test

---
+++ PascalTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PascalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valor N = ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            int final = Prova(n);
+
+            Console.WriteLine("Final: " + final);
+
+            Console.ReadLine();
+        }
+
+
+        public static int Prova(int n)
+        {
+            int valor = 0;
+
+            if (n != 0)
+                valor = n * 2 - 1 + Prova(n - 1);
+
+            Console.WriteLine(valor.ToString());
+
+            return valor;
+        }
+    }
+}
